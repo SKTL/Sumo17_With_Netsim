b95fa12edaf7213ffab1d4193911a2e7ab83ecfb
Rudhir Upretee <upretee.rudhir@gmail.com>
2013-05-03 16:33:10 -0400

    Initial Commit

---
+++ tools/sumolib/output/convert/__init__.py
@@ -0,0 +1,15 @@
+"""
+@file    __init__.py
+@author  Daniel Krajzewicz
+@date    2012-12-04
+@version $Id: __init__.py 13845 2013-05-02 13:53:19Z dkrajzew $
+
+This file includes all shape structures.
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2008-2013 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+
+import phem, omnet, ns2, shawn, gpsdat
+
